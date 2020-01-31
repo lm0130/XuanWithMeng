91c4cb910ed4a0ce644af2ecf469cbb929cacbd6
yyx11 <yyx112358@163.com>
2020-01-31 18:29:49 +0800

    1.python爬虫修改：加入少许反爬措施 2.javascript地图显示：study.html加入绘制点标记功能

--- 租房信息查询/Lm-InformationDownload.py
+++ 租房信息查询/Lm-InformationDownload.py
@@ -3,13 +3,20 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 import re
+import time
+import random
+import fake_useragent
 
 # TODO:加入一些参数：地区、租金、整套or单间、是否显示调试信息等
 base_url = 'https://beijing.baixing.com/zhengzu'
+ua = fake_useragent.UserAgent()  # 反爬措施1：使用随机User Agent
+
+f = open(r"Offline/information.csv", 'w', encoding='utf-8', newline='')  # 加入newline=''防止出现多余空行
+
 for page_num in range(1, 3):  # TODO:将搜索页数改成命令行参数
     # web_url=requests.get(base_url+'/?page=',params=page_num)
     total_url = base_url + '/?page=' + str(page_num)
-    web_url = requests.get(total_url)
+    web_url = requests.get(total_url, headers={'User-Agent': ua.random})
     print(web_url.url)
 
     html_test = web_url.text
@@ -21,7 +28,7 @@ for page_num in range(1, 3):  # TODO:将搜索页数改成命令行参数
     print(type(soup.a))
     # print(soup.a.get('ad-title'))
 
-    f = open(r"Offline/information.csv", 'w', encoding='utf-8', newline='')  # 加入newline=''防止出现多余空行
+
     csv_writer = csv.writer(f)
 
     house_name_list = soup.select(".ad-title")
@@ -34,7 +41,8 @@ for page_num in range(1, 3):  # TODO:将搜索页数改成命令行参数
         # 获取经纬度（打开页面，页面内部将写出。格式为 lng = 116.46125350856, lat = 39.879178775689）
         # 返回值为[经度，纬度]，查找失败则为['','']
         location = ['', '']
-        locationStr = requests.get(house_url)
+
+        locationStr = requests.get(house_url, headers={'User-Agent': ua.random})
         if locationStr:
             locationStr = locationStr.text
             locationStr = re.search(r'lng = \d*\.\d*, lat = \d*\.\d*', locationStr)  # 找到对应字符串
@@ -45,12 +53,15 @@ for page_num in range(1, 3):  # TODO:将搜索页数改成命令行参数
         houseInfo = [house_name, location[0], location[1], house_price_number, house_url]
         print("\n".join(houseInfo))
         csv_writer.writerow(houseInfo)
+        time.sleep(random.randrange(500, 2000) / 1000)  # 反爬虫破解2：随机休息时间
 
-    f.close()
-    '''
+    time.sleep(random.randrange(1500, 3000) / 1000)
+
+f.close()
+'''
     print("3===============================")
     for house_price in house_price_list:
         print(house_price.next)
         house_price_number=house_price.next
         print(house_price)
-    '''
+'''
