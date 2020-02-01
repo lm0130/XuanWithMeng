730710855583d64c5052b40a99523cd3d1de07cc
lm0130 <meng_li0130@163.com>
2020-02-01 18:29:00 +0800

    增加租房信息和位置-部分写完

--- 租房信息查询/Lm-InformationDownload.py
+++ 租房信息查询/Lm-InformationDownload.py
@@ -6,6 +6,7 @@ import re
 import time
 import random
 import fake_useragent
+import lxml
 
 # TODO:加入一些参数：地区、租金、整套or单间、是否显示调试信息等
 base_url = 'https://beijing.baixing.com/zhengzu'
@@ -13,7 +14,7 @@ ua = fake_useragent.UserAgent()  # 反爬措施1：使用随机User Agent
 
 f = open(r"Offline/information.csv", 'w', encoding='utf-8', newline='')  # 加入newline=''防止出现多余空行
 
-for page_num in range(1, 3):  # TODO:将搜索页数改成命令行参数
+for page_num in range(1, 2):  # TODO:将搜索页数改成命令行参数
     # web_url=requests.get(base_url+'/?page=',params=page_num)
     total_url = base_url + '/?page=' + str(page_num)
     web_url = requests.get(total_url, headers={'User-Agent': ua.random})
@@ -33,11 +34,18 @@ for page_num in range(1, 3):  # TODO:将搜索页数改成命令行参数
 
     house_name_list = soup.select(".ad-title")
     house_price_list = soup.select(".highlight")
+    house_loc_list = soup.select(".ad-item-detail")
+    flag=0;
+    i=0
     for house, house_price in zip(house_name_list, house_price_list):
         house_name = house.get_text()  # 标题
+        house_info_item =house_loc_list[i].get_text()
+        house_loc_item = house_loc_list[i+1].get_text()
+        i+=2
         house_url = house['href']  # 链接
         house_price_number = house_price.next[:-1]  # 价格。需去掉末尾“元”字
 
+        '''
         # 获取经纬度（打开页面，页面内部将写出。格式为 lng = 116.46125350856, lat = 39.879178775689）
         # 返回值为[经度，纬度]，查找失败则为['','']
         location = ['', '']
@@ -50,7 +58,9 @@ for page_num in range(1, 3):  # TODO:将搜索页数改成命令行参数
                 location = re.findall(r'\d*\.\d*', locationStr.group())  # 取出对应的数字
 
         # TODO:完善一些信息：整套or单间，房间大小，房源添加日期，房源信息等
-        houseInfo = [house_name, location[0], location[1], house_price_number, house_url]
+        houseInfo = [house_name, location[0], location[1], house_price_number, house_url]        
+        '''
+        houseInfo = [house_name, "-----",house_price_number,"------",house_loc_item,"-----",house_info_item,house_url]
         print("\n".join(houseInfo))
         csv_writer.writerow(houseInfo)
         time.sleep(random.randrange(500, 2000) / 1000)  # 反爬虫破解2：随机休息时间
