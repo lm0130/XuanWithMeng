4cddf57124a64d14bc616c7b248ecf4d4104ea0f
lm0130 <meng_li0130@163.com>
2020-02-02 13:18:51 +0800

    新增区域选择和价格选择功能

--- 租房信息查询/Lm-InformationDownload.py
+++ 租房信息查询/Lm-InformationDownload.py
@@ -8,25 +8,43 @@ import random
 import fake_useragent
 import lxml
 
+def url_func(dic,base_url,area_dic,page_num):
+    if(dic["area"]!="全北京"):
+        url_1=base_url+area_dic[dic["area"]]
+    else:
+        url_1=base_url
+    lower_bound=dic["lower bound"] if int(dic["lower bound"])>0 else ""
+    upper_bound=dic["upper bound"] if int(dic["upper bound"])>0 else ""
+    url_2=url_1+'page='+str(page_num)+'%E4%BB%B7%E6%A0%BC%5B0%5D='+lower_bound+'&%E4%BB%B7%E6%A0%BC%5B1%5D='+upper_bound+'&query='
+    return url_2
+
+
 # TODO:加入一些参数：地区、租金、整套or单间、是否显示调试信息等
 base_url = 'https://beijing.baixing.com/zhengzu'
+area_dic={"朝阳":"/m7294/?","海淀":"/m7295/?","丰台":"/m7296/?","通州":"/m7297"}
 ua = fake_useragent.UserAgent()  # 反爬措施1：使用随机User Agent
 
 f = open(r"Offline/information.csv", 'w', encoding='utf-8', newline='')  # 加入newline=''防止出现多余空行
 
+#传入格式：字典类型["area":"海淀","lower bound":"2000","upper bound":"4000"]
+#         若价格不限或者为["area":"海淀","lower bound":"-1","upper bound":"-1"]
+
 for page_num in range(1, 2):  # TODO:将搜索页数改成命令行参数
     # web_url=requests.get(base_url+'/?page=',params=page_num)
-    total_url = base_url + '/?page=' + str(page_num)
+    #total_url = base_url + '/?page=' + str(page_num)
+    dic={"area":"朝阳","lower bound":"-1","upper bound":"4000"}
+    total_url=url_func(dic,base_url,area_dic,page_num)
+    print(total_url)
     web_url = requests.get(total_url, headers={'User-Agent': ua.random})
     print(web_url.url)
 
     html_test = web_url.text
     soup = BeautifulSoup(html_test, features='lxml')
-    print(soup, type(soup))
-    print("===============================")
-    print(soup.span)
-    print("2===============================")
-    print(type(soup.a))
+    #print(soup, type(soup))
+    #print("===============================")
+    #print(soup.span)
+    #print("2===============================")
+    #print(type(soup.a))
     # print(soup.a.get('ad-title'))
 
 
@@ -60,8 +78,8 @@ for page_num in range(1, 2):  # TODO:将搜索页数改成命令行参数
         # TODO:完善一些信息：整套or单间，房间大小，房源添加日期，房源信息等
         houseInfo = [house_name, location[0], location[1], house_price_number, house_url]        
         '''
-        houseInfo = [house_name, "-----",house_price_number,"------",house_loc_item,"-----",house_info_item,house_url]
-        print("\n".join(houseInfo))
+        houseInfo = [house_name,house_price_number,house_loc_item,house_info_item,house_url]
+        #print("\n".join(houseInfo))
         csv_writer.writerow(houseInfo)
         time.sleep(random.randrange(500, 2000) / 1000)  # 反爬虫破解2：随机休息时间
 
